857ec803863871242ca1d13b32138b193ab259cc
José Ferraz-Caetano <124404174+jfcaetano@users.noreply.github.com>
2023-12-07 20:14:46 +0000

    Add files via upload

---
+++ ABM Model/3 Methods/agent-3-0125.py
@@ -0,0 +1,97 @@
+from mesa import Agent
+from belief import Belief, Mode
+import pandas as pd
+
+class PopAgent(Agent):
+    """An Agent with some initial knowledge."""
+
+    def __init__(self, unique_id, model, neighbors, sharetime):
+        super().__init__(unique_id, model)
+
+        # Default params
+        self.belief = Belief.Neutral
+        self.neighbors = neighbors  # list of agent's neighbours
+        self.clock = 0  # internal timer (absolute time)
+        self.beliefTime = 0  # time current belief has been held
+        self.shareTime = sharetime   # time limit within which new beliefs are shared
+
+
+    def tick(self):
+        """Increment clock by 1."""
+        self.clock += 1
+        self.beliefTime += 1
+
+    def isSharing(self):
+        """Check if agent is still sharing own belief."""
+        return self.beliefTime <= self.shareTime
+
+    def setBelief(self, belief):
+        """Set agent's belief."""
+        self.belief = belief
+        self.beliefTime = 0
+
+    def update(self, other):
+        """Update agent's own beliefs"""
+
+        # Convert self to Method_A belief
+        if self.belief == Belief.Neutral and other.belief == Belief.Method_A:
+            self.setBelief(Belief.Method_A)
+            
+                
+        if self.belief == Belief.Neutral and other.belief == Belief.Method_B:
+            self.setBelief(Belief.Method_B)
+            
+                
+        if self.belief == Belief.Neutral and other.belief == Belief.Method_C:
+            self.setBelief(Belief.Method_C)
+
+        # Convert self to Method_B belief
+        if self.belief == Belief.Method_C and other.belief == Belief.Method_A and self.beliefTime > 50 and self.beliefTime < 100:
+            self.setBelief(Belief.Method_A)
+            
+        if self.belief == Belief.Method_B and other.belief == Belief.Method_A and self.beliefTime > 50 and self.beliefTime < 100:
+            self.setBelief(Belief.Method_A)
+            
+        if self.belief == Belief.Neutral and other.belief == Belief.Method_A and self.beliefTime > 50 and self.beliefTime < 100:
+            self.setBelief(Belief.Method_A)
+            
+        if self.belief == Belief.Method_A and other.belief == Belief.Method_B  and self.beliefTime > 100 and self.beliefTime < 200:
+            self.setBelief(Belief.Method_B)
+            
+        if self.belief == Belief.Method_C and other.belief == Belief.Method_B and self.beliefTime > 100 and self.beliefTime < 200:
+            self.setBelief(Belief.Method_B)
+            
+        if self.belief == Belief.Neutral and other.belief == other.belief == Belief.Method_B and self.beliefTime > 100 and self.beliefTime < 200:
+            self.setBelief(Belief.Method_B)
+            
+        if self.belief == Belief.Method_A and other.belief == Belief.Method_C  and self.beliefTime > 200 and self.beliefTime < 250:
+            self.setBelief(Belief.Method_C)
+            
+        if self.belief == Belief.Method_B and other.belief == Belief.Method_C and self.beliefTime > 200 and self.beliefTime < 250:
+            self.setBelief(Belief.Method_C)
+            
+        if self.belief == Belief.Neutral and other.belief == other.belief == Belief.Method_C and self.beliefTime > 200 and self.beliefTime < 250:
+            self.setBelief(Belief.Method_C)
+
+        if self.belief == Belief.Method_A and other.belief == Belief.Method_B  and self.beliefTime > 250 and self.beliefTime < 300:
+            self.setBelief(Belief.Method_B)
+            
+        if self.belief == Belief.Method_C and other.belief == Belief.Method_B and self.beliefTime > 250 and self.beliefTime < 300:
+            self.setBelief(Belief.Method_B)
+            
+        if self.belief == Belief.Neutral and other.belief == other.belief == Belief.Method_B and self.beliefTime > 250 and self.beliefTime < 300:
+            self.setBelief(Belief.Method_B)
+
+        if self.belief == Belief.Method_A and other.belief == Belief.Method_C  and self.beliefTime > 300:
+            self.setBelief(Belief.Method_C)
+            
+        if self.belief == Belief.Method_B and other.belief == Belief.Method_C and self.beliefTime > 300:
+            self.setBelief(Belief.Method_C)
+            
+        if self.belief == Belief.Neutral and other.belief == other.belief == Belief.Method_C and self.beliefTime > 300:
+            self.setBelief(Belief.Method_C)
+            
+
+    def step(self, interlocutor):
+        """Interact with interlocutor, updating own beliefs."""
+        self.update(interlocutor)
---
+++ ABM Model/3 Methods/model.py
@@ -0,0 +1,91 @@
+from mesa import Model
+from agent import PopAgent, Belief
+from simpleScheduler import SimpleActivation
+import random
+
+
+class KnowledgeModel(Model):
+    """A model with some number of agents."""
+
+    def __init__(self, network, sharingMode, sharetime, delay, singleSource=False, samePartition=True):
+        self.mode = sharingMode
+        self.G = network
+        self.num_agents = self.G.number_of_nodes()
+        self.schedule = SimpleActivation(self)
+        self.delay = delay
+        self.singleSource = singleSource
+        self.samePartition = samePartition
+
+        # Create agents
+        for i in range(self.num_agents):
+            neighbors = list(self.G.neighbors(i))
+            a = PopAgent(unique_id=i, model=self, neighbors=neighbors, sharetime=sharetime)
+
+            if i == 0:
+                # Give Agent 0 Method_A information
+                a.belief = Belief.Method_A
+                self.agentZero = a
+            if (i == 1) and (self.delay == 0) and self.samePartition is None and self.singleSource:
+                # Give Agent 1 true information
+                a.belief = Belief.Method_B
+            if (i == 2) and (self.delay == 0) and self.samePartition is None and self.singleSource:
+                # Give Agent 2 Method_C information
+                a.belief = Belief.Method_C
+                
+            self.schedule.add(a)
+
+        if (self.delay == 0) and (self.samePartition is not None or not self.singleSource):
+            self.addMethod_B()
+            
+        if (self.delay == 0) and (self.samePartition is not None or not self.singleSource):
+            self.addMethod_C()
+
+    def addMethod_B(self):
+        """Add Method_B belief to random agent."""
+        if self.singleSource:
+            a = self.agentZero
+        elif self.samePartition is None:
+            a = random.choice(self.schedule.agents)
+        elif 'partition' in self.G.graph.keys() and self.samePartition:
+            if (self.delay == 0):
+                num = random.choice(list(self.G.graph['partition'][0])[1:])  # retraction source != Method_A source
+            else:
+                num = random.choice(list(self.G.graph['partition'][0]))
+            a = self.schedule.agents[num]
+        elif 'partition' in self.G.graph.keys() and not self.samePartition:
+            num = random.choice(list(self.G.graph['partition'][1]))
+            a = self.schedule.agents[num]
+        else:
+            a = random.choice(self.schedule.agents)
+        a.setBelief(Belief.Method_B)
+        
+        
+    def addMethod_C(self):
+        """Add Method_C belief to random agent."""
+        if self.singleSource:
+            a = self.agentZero
+        elif self.samePartition is None:
+            a = random.choice(self.schedule.agents)
+        elif 'partition' in self.G.graph.keys() and self.samePartition:
+            if (self.delay == 0):
+                num = random.choice(list(self.G.graph['partition'][0])[1:])  # retraction source != Method_A source
+            else:
+                num = random.choice(list(self.G.graph['partition'][0]))
+            a = self.schedule.agents[num]
+        elif 'partition' in self.G.graph.keys() and not self.samePartition:
+            num = random.choice(list(self.G.graph['partition'][1]))
+            a = self.schedule.agents[num]
+        else:
+            a = random.choice(self.schedule.agents)
+        a.setBelief(Belief.Method_C)
+
+    def step(self):
+        """Advance the model by one step."""
+        if (self.delay > 0) and (self.schedule.time == self.delay):
+            self.addMethod_B()
+
+        self.schedule.step()
+
+    def logs(self):
+        """Get agents' belief, interaction and pair history, respectively."""
+        return self.schedule.logs()
